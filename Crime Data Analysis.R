3fd1a1729ba85150af9a4d60f8f85d6a06a42a39
Lauren Ansell <lauren.adams@plymouth.ac.uk>
2026-08-06 13:26:24 +0100

    Devon and Cornwall data filtered to only include the area of interest and Month variable converted to a date to use if required.

--- Crime Data Analysis.R
+++ Crime Data Analysis.R
@@ -99,3 +99,35 @@ plymouth_lsoas<-c("Plymouth 034A", "Plymouth 034B", "Plymouth 034C", "Plymouth 0
 crime_data_street_plymouth<-crime_data_street_all |> filter(LSOA.name %in% plymouth_lsoas)
 
 crime_data_outcomes_plymouth<-crime_data_outcomes_all |> filter(LSOA.name %in% plymouth_lsoas)
+
+## Use the latitude and longitudes from the subsets above to filter the stop and search data
+
+max(crime_data_street_plymouth$Longitude) ## -4.144637
+min(crime_data_street_plymouth$Longitude) ## -4.164925
+
+max(crime_data_street_plymouth$Latitude) ## 50.37173
+min(crime_data_street_plymouth$Latitude) ## 50.36098
+
+## Check for any differences with the outcome data
+
+max(crime_data_outcomes_plymouth$Longitude) ## -4.144637
+min(crime_data_outcomes_plymouth$Longitude) ## -4.164925
+
+max(crime_data_outcomes_plymouth$Latitude) ## 50.37173
+min(crime_data_outcomes_plymouth$Latitude) ## 50.36098
+
+## Both the same
+
+crime_data_stopandsearch_plymouth<-crime_data_stopandsearch_all |> filter(Latitude>50.36098 & Latitude<50.37173) |> 
+  filter(Longitude>-4.164925 & Longitude< -4.144637)
+
+## Convert some of the data types
+
+str(crime_data_street_plymouth)
+
+crime_data_street_plymouth$Month<-as.Date(paste(crime_data_street_plymouth$Month, "-01", sep=""))
+
+
+## Street level analysis
+
+crime_data_street_plymouth |> group_by()
