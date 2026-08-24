5492922aafa1ac260f9fca07fd37be8437337a18
Lauren Ansell <lauren.adams@plymouth.ac.uk>
2026-08-24 11:15:48 +0100

    bar plots of assets created

--- Community Life Survey Analysis.R
+++ Community Life Survey Analysis.R
@@ -12,7 +12,7 @@
 ## Modified on: 11/08/2026
 ##              12/08/2026
 ##              20/08/2026
-
+##              24/08/2026
 
 ## Load in the required libraries
 library(tidyverse)
@@ -256,53 +256,164 @@ Hastings_CLS_assets<-Hastings_CLS_reduced |> select(Assets2A_a,Assets2A_b,Assets
 
 Hastings_CLS_assets <- Hastings_CLS_assets %>% replace(is.na(.), 0)
 
-## group the responses depending on which assets they are assoicated with
+## group the responses depending on which assets they are associated with
+
+distance <- c("15-20 mintues", "Further but still local","None","Not sure/Don't know")
 
 ## General/grocery shop (Assets2A)
 
 hastings_generalshop<-Hastings_CLS_assets |> select(Assets2A_a,Assets2A_b,Assets2A_c,Assets2A_d)
 
+hastings_generalshop_total<-colSums(hastings_generalshop)
+
+generalshop_hastings<-cbind(distance,hastings_generalshop_total) |> as.data.frame()
+
+generalshop_hastings$hastings_generalshop_total<-as.numeric(generalshop_hastings$hastings_generalshop_total)
+
+ggplot(generalshop_hastings,aes(x=distance,y=hastings_generalshop_total))+geom_bar(stat="identity")+
+  coord_flip()
+  
+
 ## Pub/bar (Assets2B)
 
 hastings_bar<-Hastings_CLS_assets |> select(Assets2B_a,Assets2B_b,Assets2B_c,Assets2B_d)
+
+hastings_bar_total<-colSums(hastings_bar)
+
+bar_hastings<-cbind(distance,hastings_bar_total) |> as.data.frame()
+
+bar_hastings$hastings_bar_total<-as.numeric(bar_hastings$hastings_bar_total)
+
+ggplot(bar_hastings,aes(x=distance,y=hastings_bar_total))+geom_bar(stat="identity")+
+  coord_flip()
 
 ## Park (Assets2C)
 
 hastings_park<-Hastings_CLS_assets |> select(Assets2C_a,Assets2C_b,Assets2C_c,Assets2C_d)
 
+hastings_park_total<-colSums(hastings_park)
+
+park_hastings<-cbind(distance,hastings_park_total) |> as.data.frame()
+
+park_hastings$hastings_park_total<-as.numeric(park_hastings$hastings_park_total)
+
+ggplot(park_hastings,aes(x=distance,y=hastings_park_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Library (Assets2D)
 
 hastings_library<-Hastings_CLS_assets |> select(Assets2D_a,Assets2D_b,Assets2D_c,Assets2D_d)
+
+hastings_library_total<-colSums(hastings_library)
+
+library_hastings<-cbind(distance,hastings_library_total) |> as.data.frame()
+
+library_hastings$hastings_library_total<-as.numeric(library_hastings$hastings_library_total)
+
+ggplot(library_hastings,aes(x=distance,y=hastings_library_total))+geom_bar(stat="identity")+
+  coord_flip()
 
 ## Restaurant/cafe (Assets2E)
 
 hastings_cafe<-Hastings_CLS_assets |> select(Assets2E_a,Assets2E_b,Assets2E_c,Assets2E_d)
 
+hastings_cafe_total<-colSums(hastings_cafe)
+
+cafe_hastings<-cbind(distance,hastings_cafe_total) |> as.data.frame()
+
+cafe_hastings$hastings_cafe_total<-as.numeric(cafe_hastings$hastings_cafe_total)
+
+ggplot(cafe_hastings,aes(x=distance,y=hastings_cafe_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Community centre/hall (Assets2F)
 
 hastings_communityhall<-Hastings_CLS_assets |> select(Assets2F_a,Assets2F_b,Assets2F_c,Assets2F_d)
 
+hastings_communityhall_total<-colSums(hastings_communityhall)
+
+communityhall_hastings<-cbind(distance,hastings_communityhall_total) |> as.data.frame()
+
+communityhall_hastings$hastings_communityhall_total<-as.numeric(communityhall_hastings$hastings_communityhall_total)
+
+ggplot(communityhall_hastings,aes(x=distance,y=hastings_communityhall_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Sports facilities (Assets2G)
 
-hastings_sportsfacilites<-Hastings_CLS_assets |> select(Assets2G_a,Assets2G_b,Assets2G_c,Assets2G_d)
+hastings_sportsfacilities<-Hastings_CLS_assets |> select(Assets2G_a,Assets2G_b,Assets2G_c,Assets2G_d)
+
+hastings_sportsfacilities_total<-colSums(hastings_sportsfacilities)
+
+sportsfacilities_hastings<-cbind(distance,hastings_sportsfacilities_total) |> as.data.frame()
+
+sportsfacilities_hastings$hastings_sportsfacilities_total<-as.numeric(sportsfacilities_hastings$hastings_sportsfacilities_total)
+
+ggplot(sportsfacilities_hastings,aes(x=distance,y=hastings_sportsfacilities_total))+geom_bar(stat="identity")+
+  coord_flip()
 
 ## Health centre/GP (Assets2H)
 
 hastings_gp<-Hastings_CLS_assets |> select(Assets2H_a,Assets2H_b,Assets2H_c,Assets2H_d)
 
+hastings_gp_total<-colSums(hastings_gp)
+
+gp_hastings<-cbind(distance,hastings_gp_total) |> as.data.frame()
+
+gp_hastings$hastings_gp_total<-as.numeric(gp_hastings$hastings_gp_total)
+
+ggplot(gp_hastings,aes(x=distance,y=hastings_gp_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Chemist or pharmacy (Assets2I)
 
 hastings_chemist<-Hastings_CLS_assets |> select(Assets2I_a,Assets2I_b,Assets2I_c,Assets2I_d)
+
+hastings_chemist_total<-colSums(hastings_chemist)
+
+chemist_hastings<-cbind(distance,hastings_chemist_total) |> as.data.frame()
+
+chemist_hastings$hastings_chemist_total<-as.numeric(chemist_hastings$hastings_chemist_total)
+
+ggplot(chemist_hastings,aes(x=distance,y=hastings_chemist_total))+geom_bar(stat="identity")+
+  coord_flip()
 
 ## Post Office (Assets2J)
 
 hastings_postoffice<-Hastings_CLS_assets |> select(Assets2J_a,Assets2J_b,Assets2J_c,Assets2J_d)
 
+hastings_postoffice_total<-colSums(hastings_postoffice)
+
+postoffice_hastings<-cbind(distance,hastings_postoffice_total) |> as.data.frame()
+
+postoffice_hastings$hastings_postoffice_total<-as.numeric(postoffice_hastings$hastings_postoffice_total)
+
+ggplot(postoffice_hastings,aes(x=distance,y=hastings_postoffice_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Place of worship for my faith or religion, such as a church, mosque, temple (Assets2K)
 
 hastings_worship<-Hastings_CLS_assets |> select(Assets2K_a,Assets2K_b,Assets2K_c,Assets2K_d)
 
+hastings_worship_total<-colSums(hastings_worship)
+
+worship_hastings<-cbind(distance,hastings_worship_total) |> as.data.frame()
+
+worship_hastings$hastings_worship_total<-as.numeric(worship_hastings$hastings_worship_total)
+
+ggplot(worship_hastings,aes(x=distance,y=hastings_worship_total))+geom_bar(stat="identity")+
+  coord_flip()
+
 ## Public transport links (Assets2L)	
 
 hastings_publictransport<-Hastings_CLS_assets |> select(Assets2L_a,Assets2L_b,Assets2L_c,Assets2L_d)
+
+hastings_publictransport_total<-colSums(hastings_publictransport)
+
+publictransport_hastings<-cbind(distance,hastings_publictransport_total) |> as.data.frame()
+
+publictransport_hastings$hastings_publictransport_total<-as.numeric(publictransport_hastings$hastings_publictransport_total)
+
+ggplot(publictransport_hastings,aes(x=distance,y=hastings_publictransport_total))+geom_bar(stat="identity")+
+  coord_flip()
 
