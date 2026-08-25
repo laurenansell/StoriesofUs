48fcd0c400d0b52de6a9916a576baeb766b28d18
Lauren Ansell <lauren.adams@plymouth.ac.uk>
2026-08-25 11:54:36 +0100

    CLS analysis for Plymouth added

--- Community Life Survey Analysis.R
+++ Community Life Survey Analysis.R
@@ -13,6 +13,7 @@
 ##              12/08/2026
 ##              20/08/2026
 ##              24/08/2026
+##              25/08/2026
 
 ## Load in the required libraries
 library(tidyverse)
@@ -55,6 +56,8 @@ rm(data)
 ##  CivAct21 (-28)
 ##  Assets2A_a (-Assets2L_d)
 ##  LocInvNa (-LocInvNg)
+
+## Hastings
 
 Hastings_CLS_reduced<-Hastings_CLS |> select(SBeNeigh,SPull,SPullL,LocAtt,FUnPd1A,FUnPd1B,FUnPd1C,FUnPd1D,FUnPd1E,FUnPd1F,
                                              FUnPd1G,FUnPd1H,FUnPd1I,FUnPd1J,FUnPd1K,FUnPd1L,FUnPd1M,FUnPd1N,
@@ -415,5 +418,370 @@ publictransport_hastings<-cbind(distance,hastings_publictransport_total) |> as.d
 publictransport_hastings$hastings_publictransport_total<-as.numeric(publictransport_hastings$hastings_publictransport_total)
 
 ggplot(publictransport_hastings,aes(x=distance,y=hastings_publictransport_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+
+
+## Plymouth
+
+Plymouth_CLS_reduced<-Plymouth_CLS |> select(SBeNeigh,SPull,SPullL,LocAtt,FUnPd1A,FUnPd1B,FUnPd1C,FUnPd1D,FUnPd1E,FUnPd1F,
+                                             FUnPd1G,FUnPd1H,FUnPd1I,FUnPd1J,FUnPd1K,FUnPd1L,FUnPd1M,FUnPd1N,
+                                             CivAct21,CivAct22,CivAct23,CivAct24,CivAct25,CivAct26,CivAct27,CivAct28,
+                                             Assets2A_a,Assets2A_b,Assets2A_c,Assets2A_d,Assets2B_a,Assets2B_b,
+                                             Assets2B_c,Assets2B_d,Assets2C_a,Assets2C_b,Assets2C_c,Assets2C_d,
+                                             Assets2D_a,Assets2D_b,Assets2D_c,Assets2D_d,Assets2E_a,Assets2E_b,
+                                             Assets2E_c,Assets2E_d,Assets2F_a,Assets2F_b,Assets2F_c,Assets2F_d,
+                                             Assets2G_a,Assets2G_b,Assets2G_c,Assets2G_d,Assets2H_a,Assets2H_b,
+                                             Assets2H_c,Assets2H_d,Assets2I_a,Assets2I_b,Assets2I_c,Assets2I_d,
+                                             Assets2J_a,Assets2J_b,Assets2J_c,Assets2J_d,Assets2K_a,Assets2K_b,
+                                             Assets2K_c,Assets2K_d,Assets2K_e,Assets2L_a,Assets2L_b,Assets2L_c,
+                                             Assets2L_d,
+                                             LocInvNa,LocInvNb,LocInvNc,LocInvNd,LocInvNe,LocInvNf,LocInvNg,lad25cd)
+
+## Save data for the explainer packs
+
+
+write.csv(Plymouth_CLS_reduced,"../Data for Explainer Pack/Plymouth_cls.csv",row.names = FALSE)
+
+table(Plymouth_CLS_reduced$SBeNeigh)
+
+SBeNeigh_f<-Plymouth_CLS_reduced$SBeNeigh |> as.factor() |> as.data.frame()
+
+
+names(SBeNeigh_f) <- "How strongly do you feel you belong to your immediate neighbourhood"
+
+
+lik <- likert(items=SBeNeigh_f[,1,drop=FALSE])
+
+plot(lik)
+
+
+Plymouth_CLS_reduced |> group_by(SPull) |> count() |> 
+  ggplot(aes(x=SPull,y=n))+geom_bar(stat="identity")
+
+
+## Pull together the multiple answer questions
+
+## Unpaid help
+
+Plymouth_CLS_unpaidhelp<-Plymouth_CLS_reduced |> select(FUnPd1A,FUnPd1B,FUnPd1C,FUnPd1D,FUnPd1E,FUnPd1F,
+                                                        FUnPd1G,FUnPd1H,FUnPd1I,FUnPd1J,FUnPd1K,FUnPd1L,
+                                                        FUnPd1M,FUnPd1N)
+
+## Lots of NAs
+
+Plymouth_CLS_unpaidhelp<-Plymouth_CLS_unpaidhelp[complete.cases(Plymouth_CLS_unpaidhelp), ]
+
+
+## Removing all the NAs leaves a total of 276 observations.
+
+## The total organisations that people support
+
+Plymouth_CLS_unpaidhelp$total<-rowSums(Plymouth_CLS_unpaidhelp[,1:13]) ## don't include none of the above
+
+
+Plymouth_CLS_unpaidhelp |> group_by(total) |> count() |> 
+  ggplot(aes(x=total,y=n))+geom_bar(stat="identity") ## remove the first entry for report
+
+
+## Which is the most popular for support
+
+colSums(Plymouth_CLS_unpaidhelp)
+
+## Create a dataframe for these results
+
+help_type<-c("Raising or handling money/taking part in sponsored events",
+             "Leading a group/member of a committee",
+             "Getting other people involved",
+             "Organising or helping to run an activity or event",
+             "Visiting people",
+             "Befriending or mentoring people",
+             "Giving advice/information/counselling",
+             "Secretarial, admin or clerical work",
+             "Providing transport/driving",
+             "Representing",
+             "Campaigning",
+             "Other practical help (e.g. helping out at school, shopping)",
+             "Any other help",
+             "None of the above")
+
+Plymouth_CLS_unpaidhelp$total<-rowSums(Plymouth_CLS_unpaidhelp[,1:14])
+
+help_total<-colSums(Plymouth_CLS_unpaidhelp[,1:14])
+
+unpaid_Plymouth<-cbind(help_type,help_total) |> as.data.frame()
+
+
+ggplot(unpaid_Plymouth,aes(x=help_type,y=help_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+
+
+write.csv(Plymouth_CLS_unpaidhelp,"../Data for Explainer Pack/Plymouth_cls_unpaidhelp.csv",row.names = FALSE)
+
+## Network plot to see the relationships
+
+# Remove total and none column
+groups <- Plymouth_CLS_unpaidhelp[,1:13]
+
+# Create co-occurrence matrix
+co_mat <- t(as.matrix(groups)) %*% as.matrix(groups)
+
+# Remove self-connections
+diag(co_mat) <- 0
+
+# Convert to edge list
+edges <- as.data.frame(as.table(co_mat)) |>
+  rename(from = Var1,
+         to = Var2,
+         weight = Freq) |>
+  filter(weight > 0)
+
+# Remove duplicate edges
+edges <- edges |>
+  rowwise() |>
+  mutate(pair = paste(sort(c(from, to)), collapse = "_")) |>
+  ungroup() |>
+  distinct(pair, .keep_all = TRUE) |>
+  select(-pair)
+
+# Node frequencies
+nodes <- data.frame(
+  name = colnames(groups),
+  freq = colSums(groups)
+)
+
+# Create graph
+g <- graph_from_data_frame(
+  d = edges,
+  vertices = nodes,
+  directed = FALSE
+)
+
+# Plot
+ggraph(g, layout = "fr") +
+  geom_edge_link(aes(width = weight),
+                 alpha = 0.5,
+                 colour = "steelblue") +
+  geom_node_point(aes(size = freq),
+                  colour = "orange") +
+  geom_node_text(aes(label = name),
+                 repel = TRUE) +
+  scale_edge_width(range = c(0.5, 5)) +
+  theme_void()
+
+
+edges <- edges |>
+  filter(weight >= 3)
+
+labels <- c(
+  FUnPd1A = "Raising money",
+  FUnPd1B = "Committee member",
+  FUnPd1C = "Encouraging others",
+  FUnPd1D = "Helping to run",
+  FUnPd1E = "Visiting people",
+  FUnPd1F = "Mentoring",
+  FUnPd1G = "Giving advice",
+  FUnPd1H = "Admin",
+  FUnPd1I = "Transport",
+  FUnPd1J = "Representing",
+  FUnPd1K = "Campaigning",
+  FUnPd1L = "Other practical help",
+  FUnPd1M = "Other help"
+)
+
+nodes<-cbind(labels,nodes)
+
+nodes<-nodes |> select(labels,freq)
+
+# Plot
+ggraph(g, layout = "fr") +
+  geom_edge_link(aes(width = weight),
+                 alpha = 0.5,
+                 colour = "steelblue") +
+  geom_node_point(aes(size = freq),
+                  colour = "orange") +
+  geom_node_text(aes(label = labels),
+                 repel = TRUE) +
+  scale_edge_width(range = c(0.5, 5)) +
+  theme_void()
+
+
+## Assets information
+
+
+Plymouth_CLS_assets<-Plymouth_CLS_reduced |> select(Assets2A_a,Assets2A_b,Assets2A_c,Assets2A_d,Assets2B_a,Assets2B_b,
+                                                    Assets2B_c,Assets2B_d,Assets2C_a,Assets2C_b,Assets2C_c,Assets2C_d,
+                                                    Assets2D_a,Assets2D_b,Assets2D_c,Assets2D_d,Assets2E_a,Assets2E_b,
+                                                    Assets2E_c,Assets2E_d,Assets2F_a,Assets2F_b,Assets2F_c,Assets2F_d,
+                                                    Assets2G_a,Assets2G_b,Assets2G_c,Assets2G_d,Assets2H_a,Assets2H_b,
+                                                    Assets2H_c,Assets2H_d,Assets2I_a,Assets2I_b,Assets2I_c,Assets2I_d,
+                                                    Assets2J_a,Assets2J_b,Assets2J_c,Assets2J_d,Assets2K_a,Assets2K_b,
+                                                    Assets2K_c,Assets2K_d,Assets2K_e,Assets2L_a,Assets2L_b,Assets2L_c,
+                                                    Assets2L_d)
+
+## will have to handle the NAs differently here, change the NAs to 0.
+
+Plymouth_CLS_assets <- Plymouth_CLS_assets %>% replace(is.na(.), 0)
+
+## group the responses depending on which assets they are associated with
+
+distance <- c("15-20 mintues", "Further but still local","None","Not sure/Don't know")
+
+## General/grocery shop (Assets2A)
+
+Plymouth_generalshop<-Plymouth_CLS_assets |> select(Assets2A_a,Assets2A_b,Assets2A_c,Assets2A_d)
+
+Plymouth_generalshop_total<-colSums(Plymouth_generalshop)
+
+generalshop_Plymouth<-cbind(distance,Plymouth_generalshop_total) |> as.data.frame()
+
+generalshop_Plymouth$Plymouth_generalshop_total<-as.numeric(generalshop_Plymouth$Plymouth_generalshop_total)
+
+ggplot(generalshop_Plymouth,aes(x=distance,y=Plymouth_generalshop_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+
+## Pub/bar (Assets2B)
+
+Plymouth_bar<-Plymouth_CLS_assets |> select(Assets2B_a,Assets2B_b,Assets2B_c,Assets2B_d)
+
+Plymouth_bar_total<-colSums(Plymouth_bar)
+
+bar_Plymouth<-cbind(distance,Plymouth_bar_total) |> as.data.frame()
+
+bar_Plymouth$Plymouth_bar_total<-as.numeric(bar_Plymouth$Plymouth_bar_total)
+
+ggplot(bar_Plymouth,aes(x=distance,y=Plymouth_bar_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Park (Assets2C)
+
+Plymouth_park<-Plymouth_CLS_assets |> select(Assets2C_a,Assets2C_b,Assets2C_c,Assets2C_d)
+
+Plymouth_park_total<-colSums(Plymouth_park)
+
+park_Plymouth<-cbind(distance,Plymouth_park_total) |> as.data.frame()
+
+park_Plymouth$Plymouth_park_total<-as.numeric(park_Plymouth$Plymouth_park_total)
+
+ggplot(park_Plymouth,aes(x=distance,y=Plymouth_park_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Library (Assets2D)
+
+Plymouth_library<-Plymouth_CLS_assets |> select(Assets2D_a,Assets2D_b,Assets2D_c,Assets2D_d)
+
+Plymouth_library_total<-colSums(Plymouth_library)
+
+library_Plymouth<-cbind(distance,Plymouth_library_total) |> as.data.frame()
+
+library_Plymouth$Plymouth_library_total<-as.numeric(library_Plymouth$Plymouth_library_total)
+
+ggplot(library_Plymouth,aes(x=distance,y=Plymouth_library_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Restaurant/cafe (Assets2E)
+
+Plymouth_cafe<-Plymouth_CLS_assets |> select(Assets2E_a,Assets2E_b,Assets2E_c,Assets2E_d)
+
+Plymouth_cafe_total<-colSums(Plymouth_cafe)
+
+cafe_Plymouth<-cbind(distance,Plymouth_cafe_total) |> as.data.frame()
+
+cafe_Plymouth$Plymouth_cafe_total<-as.numeric(cafe_Plymouth$Plymouth_cafe_total)
+
+ggplot(cafe_Plymouth,aes(x=distance,y=Plymouth_cafe_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Community centre/hall (Assets2F)
+
+Plymouth_communityhall<-Plymouth_CLS_assets |> select(Assets2F_a,Assets2F_b,Assets2F_c,Assets2F_d)
+
+Plymouth_communityhall_total<-colSums(Plymouth_communityhall)
+
+communityhall_Plymouth<-cbind(distance,Plymouth_communityhall_total) |> as.data.frame()
+
+communityhall_Plymouth$Plymouth_communityhall_total<-as.numeric(communityhall_Plymouth$Plymouth_communityhall_total)
+
+ggplot(communityhall_Plymouth,aes(x=distance,y=Plymouth_communityhall_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Sports facilities (Assets2G)
+
+Plymouth_sportsfacilities<-Plymouth_CLS_assets |> select(Assets2G_a,Assets2G_b,Assets2G_c,Assets2G_d)
+
+Plymouth_sportsfacilities_total<-colSums(Plymouth_sportsfacilities)
+
+sportsfacilities_Plymouth<-cbind(distance,Plymouth_sportsfacilities_total) |> as.data.frame()
+
+sportsfacilities_Plymouth$Plymouth_sportsfacilities_total<-as.numeric(sportsfacilities_Plymouth$Plymouth_sportsfacilities_total)
+
+ggplot(sportsfacilities_Plymouth,aes(x=distance,y=Plymouth_sportsfacilities_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Health centre/GP (Assets2H)
+
+Plymouth_gp<-Plymouth_CLS_assets |> select(Assets2H_a,Assets2H_b,Assets2H_c,Assets2H_d)
+
+Plymouth_gp_total<-colSums(Plymouth_gp)
+
+gp_Plymouth<-cbind(distance,Plymouth_gp_total) |> as.data.frame()
+
+gp_Plymouth$Plymouth_gp_total<-as.numeric(gp_Plymouth$Plymouth_gp_total)
+
+ggplot(gp_Plymouth,aes(x=distance,y=Plymouth_gp_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Chemist or pharmacy (Assets2I)
+
+Plymouth_chemist<-Plymouth_CLS_assets |> select(Assets2I_a,Assets2I_b,Assets2I_c,Assets2I_d)
+
+Plymouth_chemist_total<-colSums(Plymouth_chemist)
+
+chemist_Plymouth<-cbind(distance,Plymouth_chemist_total) |> as.data.frame()
+
+chemist_Plymouth$Plymouth_chemist_total<-as.numeric(chemist_Plymouth$Plymouth_chemist_total)
+
+ggplot(chemist_Plymouth,aes(x=distance,y=Plymouth_chemist_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Post Office (Assets2J)
+
+Plymouth_postoffice<-Plymouth_CLS_assets |> select(Assets2J_a,Assets2J_b,Assets2J_c,Assets2J_d)
+
+Plymouth_postoffice_total<-colSums(Plymouth_postoffice)
+
+postoffice_Plymouth<-cbind(distance,Plymouth_postoffice_total) |> as.data.frame()
+
+postoffice_Plymouth$Plymouth_postoffice_total<-as.numeric(postoffice_Plymouth$Plymouth_postoffice_total)
+
+ggplot(postoffice_Plymouth,aes(x=distance,y=Plymouth_postoffice_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Place of worship for my faith or religion, such as a church, mosque, temple (Assets2K)
+
+Plymouth_worship<-Plymouth_CLS_assets |> select(Assets2K_a,Assets2K_b,Assets2K_c,Assets2K_d)
+
+Plymouth_worship_total<-colSums(Plymouth_worship)
+
+worship_Plymouth<-cbind(distance,Plymouth_worship_total) |> as.data.frame()
+
+worship_Plymouth$Plymouth_worship_total<-as.numeric(worship_Plymouth$Plymouth_worship_total)
+
+ggplot(worship_Plymouth,aes(x=distance,y=Plymouth_worship_total))+geom_bar(stat="identity")+
+  coord_flip()
+
+## Public transport links (Assets2L)	
+
+Plymouth_publictransport<-Plymouth_CLS_assets |> select(Assets2L_a,Assets2L_b,Assets2L_c,Assets2L_d)
+
+Plymouth_publictransport_total<-colSums(Plymouth_publictransport)
+
+publictransport_Plymouth<-cbind(distance,Plymouth_publictransport_total) |> as.data.frame()
+
+publictransport_Plymouth$Plymouth_publictransport_total<-as.numeric(publictransport_Plymouth$Plymouth_publictransport_total)
+
+ggplot(publictransport_Plymouth,aes(x=distance,y=Plymouth_publictransport_total))+geom_bar(stat="identity")+
   coord_flip()
 
